c623780e28acae5d95cbada034523b5984c51507
Md. Imrul Kayes <48817876+mdiktushar@users.noreply.github.com>
2024-11-22 14:03:00 +0600

    model binding added

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,6 +4,8 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Tymon\JWTAuth\Contracts\JWTSubject;
@@ -69,5 +71,36 @@ class User extends Authenticatable implements JWTSubject
     public function getJWTCustomClaims()
     {
         return [];
+    }
+
+
+    /**
+     * Define the relationship between the current model and the Profile model.
+     *
+     * This method defines a "has one" relationship, where the current model
+     * has one associated Profile. The foreign key for this relationship is
+     * expected to be present in the Profile model's table (typically `user_id`).
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasOne
+     */
+    public function profile(): HasOne
+    {
+        return $this->hasOne(Profile::class);
+    }
+
+
+    /**
+     * Define the relationship between the current model and the Otp model.
+     *
+     * This method defines a "has many" relationship, where the current model
+     * can have multiple associated OTP (One-Time Password) entries. The foreign key
+     * for this relationship is expected to be present in the Otp model's table
+     * (typically `user_id` or a relevant identifier).
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function otps(): HasMany
+    {
+        return $this->hasMany(Otp::class);
     }
 }
