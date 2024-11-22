2cc3f4f4d78a3ebee49071da005c9ee09bc28034
Md. Imrul Kayes <48817876+mdiktushar@users.noreply.github.com>
2024-11-22 12:23:49 +0600

    user table updated

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,10 +13,18 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('first_name');
+            $table->string('last_name');
+
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
+
             $table->string('password');
+
+            $table->string('avatar')->nullable();
+            $table->enum('role', ['user', 'helper'])->default('user');
+            $table->boolean('status')->default(true);
+
             $table->rememberToken();
             $table->timestamps();
         });
