24f2e935e085267cff6e2b17203e27022c65a42d
Md. Imrul Kayes <48817876+mdiktushar@users.noreply.github.com>
2024-12-02 10:56:54 +0600

    exception update

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -2,6 +2,9 @@
 
 use App\Helper\Helper;
 use App\Http\Middleware\EnsureGuestJwt;
+use Illuminate\Auth\Access\AuthorizationException;
+use Illuminate\Auth\AuthenticationException;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Database\QueryException;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
@@ -11,17 +14,17 @@ use Illuminate\Support\Facades\Route;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        api: __DIR__.'/../routes/api/api.php',
-        commands: __DIR__.'/../routes/console.php',
+        web: __DIR__ . '/../routes/web.php',
+        api: __DIR__ . '/../routes/api/api.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
 
         // custom routes for api
-        then:function(){
+        then: function () {
             Route::middleware([])
-            ->prefix('api/auth')
-            ->name('auth.')
-            ->group(base_path('routes/api/auth.php'));
+                ->prefix('api/auth')
+                ->name('auth.')
+                ->group(base_path('routes/api/auth.php'));
         }
     )
     ->withMiddleware(function (Middleware $middleware) {
@@ -35,8 +38,22 @@ return Application::configure(basePath: dirname(__DIR__))
                 if ($e instanceof QueryException) {
                     return Helper::error(500, 'server error', $e->getMessage());
                 }
-                return Helper::error(500, 'server error', $e->getMessage());
-            }else{
+                if ($e instanceof ModelNotFoundException) {
+                    return Helper::error(404, 'not found', $e->getMessage());
+                }
+
+                if ($e instanceof AuthenticationException) {
+                    return Helper::error(401, 'unauthorized', $e->getMessage());
+                }
+                if ($e instanceof AuthorizationException) {
+                    return Helper::error(403, 'forbidden', $e->getMessage());
+                }
+
+                // Dynamically determine the status code if available
+                $statusCode = method_exists($e, 'getStatusCode') ? $e->getStatusCode() : 500;
+                // Return a generic server error with the appropriate status code
+                return Helper::error($statusCode, 'server error', $e->getMessage());
+            } else {
                 return $e;
             }
         });
