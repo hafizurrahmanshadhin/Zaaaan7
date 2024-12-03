414cee50eb2b43539d88ab0a1acecf4009d91041
Md. Imrul Kayes <48817876+mdiktushar@users.noreply.github.com>
2024-12-03 12:48:22 +0600

    tasks

---
+++ database/migrations/2024_12_03_062539_create_addresses_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('addresses', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('category_id')->constrained('categories')->cascadeOnDelete();
+            
+            // google map
+            $table->double('latitude');
+            $table->double('longitude');
+            // manual address
+            $table->string('name');
+            $table->string('country');
+            $table->string('state')->nullable();
+            $table->string('city');
+            $table->string('zip')->comment('post code');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('addresses');
+    }
+};
---
+++ database/migrations/2024_12_03_063309_create_tasks_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('client')->comment('from users table');
+            $table->unsignedBigInteger('helper')->comment('from users table');
+
+            $table->foreignId('address_id')->constrained('addresses')->cascadeOnDelete();
+
+            $table->enum('status', ['pending', 'accepted', 'in process', 'completed'])->default('pending');
+            $table->longText('description')->comment('task description');
+            $table->date('date');
+            $table->time('time');
+
+            $table->foreign('client')->references('id')->on('users')->cascadeOnDelete();
+            $table->foreign('helper')->references('id')->on('users')->cascadeOnDelete();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tasks');
+    }
+};
