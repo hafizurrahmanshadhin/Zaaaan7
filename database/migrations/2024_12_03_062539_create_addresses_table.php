8d6450c21f157c41db6bc5964a8287b6c0ce2bb8
Md. Imrul Kayes <48817876+mdiktushar@users.noreply.github.com>
2024-12-03 17:13:31 +0600

    address migration updated

--- database/migrations/2024_12_03_062539_create_addresses_table.php
+++ database/migrations/2024_12_03_062539_create_addresses_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('addresses', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('category_id')->constrained('categories')->cascadeOnDelete();
+            $table->foreignId('user_id')->constrained('users')->cascadeOnDelete();
             
             // google map
             $table->double('latitude');
