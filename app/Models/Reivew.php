11cc913dbf60d56c8af2cf27e5441d6bd98d3e52
Md. Imrul Kayes <48817876+mdiktushar@users.noreply.github.com>
2024-12-03 16:38:06 +0600

    polimorpic relation with images table with tasks and reviews

--- app/Models/Reivew.php
+++ app/Models/Reivew.php
@@ -3,8 +3,12 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\MorphMany;
 
 class Reivew extends Model
 {
-    //
+    public function images():MorphMany
+    {
+        return $this->morphMany(Image::class, 'imageable');
+    }
 }
--- app/Models/Task.php
+++ app/Models/Task.php
@@ -3,8 +3,13 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\MorphMany;
 
 class Task extends Model
 {
-    //
+    
+    public function images():MorphMany
+    {
+        return $this->morphMany(Image::class, 'imageable');
+    }
 }
