c65716fcf8fafe41ab5cf1e81d947c53bf90a27a
Md. Imrul Kayes <48817876+mdiktushar@users.noreply.github.com>
2024-12-08 16:42:24 +0600

    create page linked

--- resources/views/backend/layouts/category/index.blade.php
+++ resources/views/backend/layouts/category/index.blade.php
@@ -40,17 +40,7 @@
                             </div>
                         </div>
                         <div class="ms-xxl-auto">
-                            <button class="btn btn-link text-body me-4 px-0">
-                                <svg class="svg-inline--fa fa-file-export fs-9 me-2" aria-hidden="true" focusable="false"
-                                    data-prefix="fas" data-icon="file-export" role="img"
-                                    xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512" data-fa-i2svg="">
-                                    <path fill="currentColor"
-                                        d="M0 64C0 28.7 28.7 0 64 0H224V128c0 17.7 14.3 32 32 32H384V288H216c-13.3 0-24 10.7-24 24s10.7 24 24 24H384V448c0 35.3-28.7 64-64 64H64c-35.3 0-64-28.7-64-64V64zM384 336V288H494.1l-39-39c-9.4-9.4-9.4-24.6 0-33.9s24.6-9.4 33.9 0l80 80c9.4 9.4 9.4 24.6 0 33.9l-80 80c-9.4 9.4-24.6 9.4-33.9 0s-9.4-24.6 0-33.9l39-39H384zm0-208H256V0L384 128z">
-                                    </path>
-                                </svg>
-                                Export
-                            </button>
-                            <button class="btn btn-primary" id="addBtn">
+                            <a href="{{route("admin.category.create")}}" class="btn btn-primary" id="addBtn">
                                 <svg class="svg-inline--fa fa-plus me-2" aria-hidden="true" focusable="false"
                                     data-prefix="fas" data-icon="plus" role="img" xmlns="http://www.w3.org/2000/svg"
                                     viewBox="0 0 448 512" data-fa-i2svg="">
@@ -58,8 +48,8 @@
                                         d="M256 80c0-17.7-14.3-32-32-32s-32 14.3-32 32V224H48c-17.7 0-32 14.3-32 32s14.3 32 32 32H192V432c0 17.7 14.3 32 32 32s32-14.3 32-32V288H400c17.7 0 32-14.3 32-32s-14.3-32-32-32H256V80z">
                                     </path>
                                 </svg>
-                                Add Product
-                            </button>
+                                Add Category
+                            </a>
                         </div>
                     </div>
                 </div>
@@ -177,7 +167,6 @@
                                             </button>
                                             <div class="dropdown-menu dropdown-menu-end py-2">
                                                 <a class="dropdown-item" href="#!">View</a>
-                                                <a class="dropdown-item" href="#!">Export</a>
                                                 <div class="dropdown-divider"></div>
                                                 <a class="dropdown-item text-danger" href="#!">Remove</a>
                                             </div>
