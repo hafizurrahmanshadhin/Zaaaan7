4c6f13bf496cd7ffc3cbb0e3dc0589767f53ef01
Md. Imrul Kayes <48817876+mdiktushar@users.noreply.github.com>
2024-12-09 18:50:17 +0600

    getting the categories

---
+++ app/Http/Controllers/API/CategoryController.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Services\API\CategoryService;
+use App\Traits\ApiResponse;
+use Exception;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
+
+class CategoryController extends Controller
+{
+    use ApiResponse;
+    protected $categoryService;
+
+    public function __construct(CategoryService $categoryService)
+    {
+        $this->categoryService = $categoryService;
+    }
+
+
+    /**
+     * Display a listing of all categories.
+     *
+     * This method handles the request to retrieve all categories from the 
+     * category service and returns them in a success response. If an error
+     * occurs, an error response is returned with the exception message.
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function index(): JsonResponse
+    {
+        try {
+            $response = $this->categoryService->getCategoryes();
+            return $this->success(200, 'getting all the categories', ['categoryes' => $response]);
+        } catch (Exception $e) {
+            Log::error('CategoryController:index ->' . $e->getMessage());
+            return $this->error(500, 'failed to get categories', $e->getMessage());
+        }
+
+    }
+
+
+    /**
+     * Display the specified category.
+     *
+     * This method handles the request to retrieve a specific category by 
+     * its identifier from the category service and returns it in a success 
+     * response. If an error occurs, an error response is returned with the 
+     * exception message.
+     *
+     * @param string $category The identifier of the category to view.
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function view($category): JsonResponse
+    {
+        try {
+            $response = $this->categoryService->getCategory($category);
+            return $this->success(200, 'getting the category', ['category' => $response]);
+        } catch (Exception $e) {
+            Log::error('CategoryController:index ->' . $e->getMessage());
+            return $this->error(500, 'failed to get the category', $e->getMessage());
+        }
+
+    }
+
+
+    /**
+     * Display the subcategories of a specific category.
+     *
+     * This method handles the request to retrieve all subcategories for a 
+     * given category and returns them in a success response. If an error 
+     * occurs, an error response is returned with the exception message.
+     *
+     * @param string $category The identifier of the category whose subcategories to retrieve.
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function subCategories($category): JsonResponse
+    {
+        try {
+            $response = $this->categoryService->getSubCategories($category);
+            return $this->success(200, 'getting sub-categories of the category', ['sub_categories' => $response]);
+        } catch (Exception $e) {
+            Log::error('CategoryController:index ->' . $e->getMessage());
+            return $this->error(500, 'failed to get sub-categories', $e->getMessage());
+        }
+
+    }
+}
---
+++ app/Services/API/CategoryService.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Services\API;
+
+use App\Models\Category;
+use App\Models\SubCategory;
+use Exception;
+
+class CategoryService
+{
+    public function __construct()
+    {
+
+    }
+
+    /**
+     * Retrieve a list of all categories.
+     *
+     * This method retrieves all categories from the database using the 
+     * Category model and returns them in an associative array. If an error 
+     * occurs, it will throw an exception to be handled by the caller.
+     *
+     * @return array
+     * @throws \Exception
+     */
+    public function getCategoryes():array
+    {
+        try {
+            $categories = Category::all();
+            return ['categories' => $categories];
+        } catch (Exception $e) {
+            throw $e;
+        }
+
+    }
+
+
+    /**
+     * Retrieve a specific category by its ID.
+     *
+     * This method retrieves a category from the database using the 
+     * Category model based on the provided ID. If the category is not 
+     * found, it will throw a ModelNotFoundException. The category data 
+     * is returned in an associative array.
+     *
+     * @param int $id The ID of the category to retrieve.
+     * @return array
+     * @throws \Illuminate\Database\Eloquent\ModelNotFoundException
+     * @throws \Exception
+     */
+    public function getCategory($id):array
+    {
+        try {
+            $category = Category::findOrFail($id);
+            return ['category' => $category];
+        } catch (Exception $e) {
+            throw $e;
+        }
+    }
+
+
+    /**
+     * Retrieve subcategories for a specific category.
+     *
+     * This method retrieves all subcategories associated with a given 
+     * category ID from the SubCategory model and returns them in an 
+     * associative array. If an error occurs, it will throw an exception.
+     *
+     * @param int $id The ID of the category to retrieve subcategories for.
+     * @return array
+     * @throws \Exception
+     */
+    public function getSubCategories($id):array
+    {
+        try {
+            $subCategoryes = SubCategory::whereCategoryId($id)->all();
+            return ['subCategoryes' => $subCategoryes];
+        } catch (Exception $e) {
+            throw $e;
+        }
+    }
+}
