1819d765d8a5dedbfd3db9e1638091b4636fd5fa
Md. Imrul Kayes <48817876+mdiktushar@users.noreply.github.com>
2024-12-15 17:12:04 +0600

    event updated

--- app/Events/MessageSent.php
+++ app/Events/MessageSent.php
@@ -3,15 +3,14 @@
 namespace App\Events;
 
 use App\Models\Message;
-use Illuminate\Broadcasting\Channel;
 use Illuminate\Broadcasting\InteractsWithSockets;
-use Illuminate\Broadcasting\PresenceChannel;
 use Illuminate\Broadcasting\PrivateChannel;
-use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcastNow;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Log;
 
-class MessageSent
+class MessageSent implements ShouldBroadcastNow
 {
     use Dispatchable, InteractsWithSockets, SerializesModels;
 
@@ -21,6 +20,7 @@ class MessageSent
      */
     public function __construct(Message $message)
     {
+       Log::info('constructor'); 
         $this->message = $message;
     }
 
@@ -31,6 +31,7 @@ class MessageSent
      */
     public function broadcastOn(): array
     {
+        Log::info('info'); 
         return [
             new PrivateChannel("chat.{$this->message->receiver_id}"),
         ];
