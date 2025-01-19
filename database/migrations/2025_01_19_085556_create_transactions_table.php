e77d50d1f3b7633563bc5e71ebb8ff8339cbd514
mdiktushar <md.tushar1804@gmail.com>
2025-01-19 15:17:20 +0600

    transaction table created

---
+++ database/migrations/2025_01_19_085556_create_transactions_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Laravel\Prompts\Table;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->id();
+            $table->string('transaction_id')->unique();
+            $table->unsignedBigInteger('client')->comment('from users table');
+            $table->unsignedBigInteger('helper')->comment('from users table');
+            $table->foreignId('task_id')->constrained('tasks')->cascadeOnDelete();
+            $table->float('amount');
+            $table->foreign('client')->references('id')->on('users')->cascadeOnDelete();
+            $table->foreign('helper')->references('id')->on('users')->cascadeOnDelete();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('transactions');
+    }
+};
