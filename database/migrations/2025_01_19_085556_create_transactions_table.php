f212e7c80b98e97329c63c2a6ac49382017018a3
mdiktushar <md.tushar1804@gmail.com>
2025-01-19 15:17:41 +0600

    transaction table created 1

--- database/migrations/2025_01_19_085556_create_transactions_table.php
+++ database/migrations/2025_01_19_085556_create_transactions_table.php
@@ -3,7 +3,6 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
-use Laravel\Prompts\Table;
 
 return new class extends Migration
 {
