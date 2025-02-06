b9260acf0754fed672fa2dc37aa2d007a58f254d
mdiktushar <md.tushar1804@gmail.com>
2025-02-06 10:35:08 +0600

    address with task

--- app/Services/API/TaskService.php
+++ app/Services/API/TaskService.php
@@ -72,7 +72,7 @@ class TaskService
     {
         try {
             $perPage = request()->query('per_page', 10);
-            $tasks = $this->user->helperTasks()->where('status', 'accepted')->with(['helper'])->paginate($perPage);
+            $tasks = $this->user->helperTasks()->where('status', 'accepted')->with(['helper', 'address'])->paginate($perPage);
             return $tasks;
         } catch (Exception $e) {
             throw $e;
@@ -88,7 +88,7 @@ class TaskService
     {
         try {
             $perPage = request()->query('per_page', 10);
-            $tasks = $this->user->helperTasks()->where('status', operator: 'completed')->with(['helper'])->paginate($perPage);
+            $tasks = $this->user->helperTasks()->where('status', operator: 'completed')->with(['helper', 'address'])->paginate($perPage);
             return $tasks;
         } catch (Exception $e) {
             throw $e;
