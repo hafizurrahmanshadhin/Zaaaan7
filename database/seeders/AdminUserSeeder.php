db2470986186060b985b7c265a1062f8407a3c70
hafizurrahmanshadhin <hr.shadhin36@gmail.com>
2025-07-01 11:48:05 +0600

    ...

--- database/seeders/AdminUserSeeder.php
+++ database/seeders/AdminUserSeeder.php
@@ -6,22 +6,19 @@ use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
 
-
-class AdminUserSeeder extends Seeder
-{
+class AdminUserSeeder extends Seeder {
     /**
      * Run the database seeds.
      */
-    public function run(): void
-    {
+    public function run(): void {
         DB::table('users')->insert([
             'first_name' => 'Admin',
-            'last_name' => 'User',
-            'handle' => 'admin',
-            'email' => 'admin@admin.com',
-            'password' => Hash::make('12345678'), // Replace 'password' with a secure password
-            'role' => 'admin',
-            'status' => true,
+            'last_name'  => 'User',
+            'handle'     => 'admin',
+            'email'      => 'admin@admin.com',
+            'password'   => Hash::make('12345678'), // Replace 'password' with a secure password
+            'role'       => 'admin',
+            'status'     => true,
             'created_at' => now(),
             'updated_at' => now(),
         ]);
--- routes/console.php
+++ routes/console.php
@@ -8,8 +8,6 @@ Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
 
-
-// retistering make service class command
 Artisan::command('make:service {name}', function ($name) {
     $this->call(MakeService::class, ['name' => $name]);
 });
