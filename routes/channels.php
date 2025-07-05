3ac0414f79258da8c07f11ffe855cc00db1aa06a
hafizurrahmanshadhin <hr.shadhin36@gmail.com>
2025-07-05 10:12:24 +0600

    ...

--- routes/channels.php
+++ routes/channels.php
@@ -7,9 +7,6 @@ Broadcast::channel('App.Models.User.{id}', function ($user, $id) {
     return (int) $user->id === (int) $id;
 });
 
-// Define a broadcast channel named "chat.{id}".
-// This channel ensures that only authenticated users with a matching ID can subscribe to the channel.
-// The callback checks if the ID of the authenticated user matches the ID in the channel name.
 Broadcast::channel('chat.{id}', function ($user, $id) {
     return (int) $user->id === (int) $id;
 });
